e9c7515427a94057626d0ee32fb088e71edbf579
ANA <Usuario@DESKTOP-UTV8S04>
2024-07-23 09:20:25 +0200

    23/07/mañana, casa

--- 14_escuela/src/main/java/dao/AlumnosDao.java
+++ 14_escuela/src/main/java/dao/AlumnosDao.java
@@ -1,11 +1,14 @@
 package dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entities.Alumno;
 
 public interface AlumnosDao extends JpaRepository <Alumno, String> {
 	
-	
+	List<Alumno> findByMatricula();
+	List<Alumno> findByFecha();
 
 }
--- 14_escuela/src/main/java/dao/CursosDao.java
+++ 14_escuela/src/main/java/dao/CursosDao.java
@@ -1,11 +1,15 @@
 package dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entities.Curso;
 
 public interface CursosDao extends JpaRepository<Curso, Integer> {
 	
+	List<Curso> findBynombre(String nombre);
+	List <Curso> cursosSinUsuario();
 	
 
 }
